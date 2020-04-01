a88e6896fbd7ac11c97020db27242bffff81d2e7
root <root@localhost.localdomain>
2020-04-01 16:01:34 +0530

    working

--- restapp/models.py
+++ restapp/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField(blank=True, default='')
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __srt__(self):  
+        return self.name
